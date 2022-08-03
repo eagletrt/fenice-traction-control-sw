21b80a2b46264713792e83a6fa26478363d5fbea
Alex Sartori <alex.sartori1997@gmail.com>
2022-08-03 23:12:36 +0200

    Add more SC manouvers

--- test_data/plot_ctrl.py
+++ test_data/plot_ctrl.py
@@ -25,7 +25,7 @@ def read_files(f_exp, f_act):
     return expected, actual
 
 
-def plot(expected, actual):
+def plot(expected, actual, t=None):
     fig, axs = plt.subplots(2, 1, sharex=True)
     axs[0].plot(expected['t_rl'], label="Expected T_rl", alpha=0.8)
     axs[0].plot(actual['t_rl'], label="Actual T_rl", alpha=0.8)
@@ -37,6 +37,10 @@ def plot(expected, actual):
     axs[1].grid()
     axs[1].legend()
 
+    if t is not None:
+        fig.suptitle(t)
+
+    plt.tight_layout()
     plt.show()
 
 
@@ -49,9 +53,11 @@ if __name__ == '__main__':
     e, a = read_files('all/output.csv', 'all/test_output.csv')
     plot(e, a)
     '''
-    e, a = read_files('test_data/slip_3/control_output_m1c2.csv', 'test_data/slip_3/control_output_m1c2_test.csv')
-    plot(e, a)
-    e, a = read_files('test_data/slip_3/control_output_m2c2.csv', 'test_data/slip_3/control_output_m2c2_test.csv')
-    plot(e, a)
-    e, a = read_files('test_data/slip_3/control_output_m8c2.csv', 'test_data/slip_3/control_output_m8c2_test.csv')
-    plot(e, a)
+    e, a = read_files('test_data/slip_4/control_output_m3c20.csv', 'test_data/slip_4/control_output_m3c20_test.csv')
+    plot(e, a, "m3c20")
+    e, a = read_files('test_data/slip_4/control_output_m3c21.csv', 'test_data/slip_4/control_output_m3c21_test.csv')
+    plot(e, a, "m3c21")
+    e, a = read_files('test_data/slip_4/control_output_m3c22.csv', 'test_data/slip_4/control_output_m3c22_test.csv')
+    plot(e, a, "m3c22")
+    e, a = read_files('test_data/slip_4/control_output_m3c23.csv', 'test_data/slip_4/control_output_m3c23_test.csv')
+    plot(e, a, "m3c23")
